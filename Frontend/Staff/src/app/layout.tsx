7bbd6b245c9ff3a602c126550b4a25e6f3025cac
acharyayush <acharyayush.official@gmail.com>
2026-02-04 16:04:10 +0545

    Add navbar to all apps

--- Frontend/Staff/src/app/layout.tsx
+++ Frontend/Staff/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import StaffNavbar from "../components/StaffNavbar"
 import Footer from "../components/Footer";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="min-h-screen flex flex-col">
+          <StaffNavbar/>
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
--- Frontend/Teacher/src/app/layout.tsx
+++ Frontend/Teacher/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TeacherNavbar from "../components/TeacherNavbar";
 import Footer from "../components/Footer";
 
 const geistSans = Geist({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="min-h-screen flex flex-col">
+          <TeacherNavbar/>
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
