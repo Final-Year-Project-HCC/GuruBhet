563ca3cda5492cf4b8dfadb3e47f60fc49235956
Ujjal Shrestha <ujjalshrestha3141592@gmail.com>
2026-04-03 21:02:02 +0545

    Add configuration to handle cookie setup in localhost as well with mkcert

--- Frontend/Staff/next.config.ts
+++ Frontend/Staff/next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from "next";
+import fs from "fs";
+import path from "path";
+
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
   reactCompiler: true,
+
+  // Use the spread operator to conditionally add the server config
+  ...(isDev && {
+    server: {
+      https: {
+        key: fs.readFileSync(
+          path.join(__dirname, "..", "shared_keys", "localhost+2-key.pem"),
+        ),
+        cert: fs.readFileSync(
+          path.join(__dirname, "..", "shared_keys", "localhost+2.pem"),
+        ),
+      },
+    },
+  }),
 };
 
 export default nextConfig;
--- Frontend/Student/next.config.ts
+++ Frontend/Student/next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+import fs from "fs";
+import path from "path";
+
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -16,6 +20,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  // Use the spread operator to conditionally add the server config
+  ...(isDev && {
+    server: {
+      https: {
+        key: fs.readFileSync(
+          path.join(__dirname, "..", "shared_keys", "localhost+2-key.pem"),
+        ),
+        cert: fs.readFileSync(
+          path.join(__dirname, "..", "shared_keys", "localhost+2.pem"),
+        ),
+      },
+    },
+  }),
 };
 
 export default nextConfig;
