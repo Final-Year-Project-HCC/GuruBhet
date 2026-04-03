0355d52057759e9758f30a39ef5c44209de0bc48
acharyayush <acharyayush.official@gmail.com>
2026-04-03 21:38:49 +0545

    change http to https for localhost

--- Frontend/Teacher/next.config.ts
+++ Frontend/Teacher/next.config.ts
@@ -1,25 +1,21 @@
 import type { NextConfig } from "next";
-import fs from "fs";
-import path from "path";
 
-const isDev = process.env.NODE_ENV === "development";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   reactCompiler: true,
-  // Use the spread operator to conditionally add the server config
-  ...(isDev && {
-    server: {
-      https: {
-        key: fs.readFileSync(
-          path.join(__dirname, "..", "shared_keys", "localhost+2-key.pem"),
-        ),
-        cert: fs.readFileSync(
-          path.join(__dirname, "..", "shared_keys", "localhost+2.pem"),
-        ),
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
       },
-    },
-  }),
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
